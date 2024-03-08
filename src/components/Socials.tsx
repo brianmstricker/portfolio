944a72792e7c70b7cce4907fa88b7eee17b093a8
brian <brianmstricker@gmail.com>
2024-03-07 21:05:26 -0500

    style: social links hover color

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -10,15 +10,22 @@ const links = [
   text: "Github",
   href: "https://github.com/brianmstricker",
   icon: <FaGithub />,
+  hoverColor: "text-slate-400",
  },
  {
   text: "LinkedIn",
   href: "https://www.linkedin.com/in/brian-stricker-a1559b250",
   icon: <FaLinkedin />,
+  hoverColor: "text-blue-500",
  },
- { text: "Email", href: "mailto::hello@brianstricker.com", icon: <LuMail /> },
+ {
+  text: "Email",
+  href: "mailto::hello@brianstricker.com",
+  icon: <LuMail />,
+  hoverColor: "text-red-500",
+ },
 ];
-
+//todo: fill color
 const Socials = () => {
  const [hovered, setHovered] = useState<string | null>(null);
  return (
@@ -31,13 +38,14 @@ const Socials = () => {
      href={link.href}
      className="transition-all duration-300 flex flex-col items-center group"
     >
-     <div className="absolute text-sm font-semibold opacity-0 group-hover:opacity-100 transition-all duration-300 top-10 group-hover:-top-2 select-none pointer-events-none bg-black px-2 py-1 rounded-lg bg-opacity-0 group-hover:bg-opacity-100">
+     <div className="absolute text-sm font-semibold opacity-0 group-hover:opacity-100 transition-all duration-300 top-9 group-hover:-top-2 select-none pointer-events-none bg-black px-2 py-1 rounded-lg bg-opacity-0 group-hover:bg-opacity-100">
       {link.text}
      </div>
      <span
       className={cn(
        "text-[28px] hover:scale-[104%] transition-all duration-300",
-       hovered && hovered !== link.text && "opacity-10"
+       hovered && hovered !== link.text && "opacity-10",
+       hovered && hovered === link.text && link.hoverColor
       )}
      >
       {link.icon}
