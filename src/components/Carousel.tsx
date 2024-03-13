d7d39ed1040f904275cd7bd2d717241e8f064178
brian <brianmstricker@gmail.com>
2024-03-13 00:42:49 -0400

    feat: carousel next/prev buttons

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Image, { StaticImageData } from "next/image";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import useEmblaCarousel from "embla-carousel-react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { cn } from "@/utils/cn";
 
 const Carousel = ({
  images,
@@ -11,26 +13,57 @@ const Carousel = ({
  projName: string;
 }) => {
  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
- // useEffect(() => {
- //  if (emblaApi) {
- //   console.log(emblaApi.slideNodes());
- //  }
- // }, [emblaApi]);
+ const scrollPrev = useCallback(() => {
+  if (emblaApi) emblaApi.scrollPrev();
+ }, [emblaApi]);
+ const scrollNext = useCallback(() => {
+  if (emblaApi) emblaApi.scrollNext();
+ }, [emblaApi]);
  return (
-  <div ref={emblaRef} className="overflow-hidden">
-   <div className="flex">
-    {images.map((image, i) => (
-     <div key={i} style={{ minWidth: 0, flex: "0 0 100%" }}>
-      <Image
-       src={image}
-       alt={projName}
-       width={2000}
-       className="object-contain"
-       sizes="(max-width: 768px) 35vw, (max-width: 1200px) 50vw, 60vw"
-      />
-     </div>
-    ))}
+  <div className="overflow-hidden relative">
+   <div ref={emblaRef}>
+    <div className="flex">
+     {images.map((image, i) => (
+      <div key={i} style={{ minWidth: 0, flex: "0 0 100%" }}>
+       <Image
+        src={image}
+        alt={projName}
+        width={2000}
+        className="object-contain"
+        sizes="(max-width: 768px) 35vw, (max-width: 1200px) 50vw, 60vw"
+       />
+      </div>
+     ))}
+    </div>
    </div>
+   <button
+    className={cn(
+     "w-fit h-fit absolute left-3 top-1/2 bottom-1/2 -translate-y-1/2 bg-black p-1.5 rounded-full opacity-20 hover:opacity-100 transition-all duration-300 ease-in-out",
+     projName === "Chirp - Twitter Clone" && "bg-white"
+    )}
+    onClick={scrollPrev}
+   >
+    <FaChevronLeft
+     className={cn(
+      "fill-white w-4 h-4 relative -left-[1px]",
+      projName === "Chirp - Twitter Clone" && "fill-black"
+     )}
+    />
+   </button>
+   <button
+    className={cn(
+     "w-fit h-fit absolute right-3 top-1/2 bottom-1/2 -translate-y-1/2 bg-black p-1.5 rounded-full opacity-20 hover:opacity-100 transition-all duration-300 ease-in-out",
+     projName === "Chirp - Twitter Clone" && "bg-white"
+    )}
+    onClick={scrollNext}
+   >
+    <FaChevronRight
+     className={cn(
+      "fill-white w-4 h-4 relative -right-[1px]",
+      projName === "Chirp - Twitter Clone" && "fill-black"
+     )}
+    />
+   </button>
   </div>
  );
 };
