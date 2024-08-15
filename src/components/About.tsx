a4aee3298640dc14ef32d2ef18d5d02b047b7a32
brian <brianmstricker@gmail.com>
2024-08-15 17:17:51 -0400

    up: p styling

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,10 +14,10 @@ const About = ({ project, section }: AboutProps) => {
       <h1 className="text-5xl md:text-6xl lg:text-7xl tracking-widest">Welcome</h1>
       <div className="w-full h-1 rounded-full bg-gradient-to-r from-yellow-200 via-yellow-300 to-orange-600" />
      </div>
-     <p className="mt-4 sm:mt-6 max-w-[43ch] sm:max-w-[37ch] lg:max-w-prose text-stone-400 mx-auto sm:mx-0 text-[15px] sm:text-lg leading-snug sm:leading-normal">
-      I&apos;m Brian Stricker, a <span className="text-white">software engineer</span> with a focus on{" "}
-      <span className="text-white">web development</span>. I&apos;m based in New Jersey, and I enjoy all aspects of development, from design
-      to deployment.
+     <p className="mt-4 sm:mt-6 max-w-[43ch] sm:max-w-[37ch] lg:max-w-prose text-stone-300 mx-auto sm:mx-0 text-[15px] sm:text-lg leading-snug sm:leading-normal">
+      I&apos;m Brian Stricker, a <span className="text-white font-extrabold">software engineer</span> with a focus on{" "}
+      <span className="text-white font-extrabold">web development</span>. I&apos;m based in New Jersey, and I enjoy all aspects of
+      development, from design to deployment.
      </p>
     </div>
    )}
